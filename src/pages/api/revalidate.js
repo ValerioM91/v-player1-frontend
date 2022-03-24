197cc0bdbd40e5811b7e996dd8716fb42282dda2
Valerio Mattera <matteravalerio@gmail.com>
2022-03-24 11:25:36 +0000

    Testing revalidate

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,6 +1,10 @@
 export default async function handler(req, res) {
-  for (const url of req.body) {
-    await res.unstable_revalidate(url);
+  try {
+    for (const url of req.body) {
+      await res.unstable_revalidate(url);
+    }
+    return res.status(200).json({ revalidate: true });
+  } catch {
+    return res.status(500).send("Error revalidating");
   }
-  res.status(200).json({ revalidate: true });
 }
