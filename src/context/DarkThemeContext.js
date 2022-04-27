3d6fca1aa35cee784d641ea7d5443b5c6532c16b
Valerio Mattera <matteravalerio@gmail.com>
2022-04-27 10:09:07 +0200

    Adding dark theme option to local storage

--- src/context/DarkThemeContext.js
+++ src/context/DarkThemeContext.js
@@ -1,16 +1,26 @@
-import React, { useContext, createContext, useState } from "react";
+import React, { useContext, createContext, useState, useEffect } from "react";
 
 const DarkThemeContext = createContext();
 
 export const DarkThemeProvider = ({ children }) => {
   const [darkTheme, setDarkTheme] = useState(false);
 
-  const toggleTheme = () => {
-    if (darkTheme === false) {
+  useEffect(() => {
+    const initialTheme = window.localStorage.getItem("darkTheme");
+    setDarkTheme(initialTheme === "true");
+  }, []);
+
+  useEffect(() => {
+    if (darkTheme) {
       document.body.style.backgroundColor = "#393939";
+      localStorage.setItem("darkTheme", true);
     } else {
       document.body.style.backgroundColor = "#fff";
+      localStorage.setItem("darkTheme", false);
     }
+  }, [darkTheme]);
+
+  const toggleTheme = () => {
     setDarkTheme((currentTheme) => !currentTheme);
   };
 
