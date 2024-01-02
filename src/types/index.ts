35a6e4600ac9e1e847bfcae2ab77559986ddfe3e
Valerio Mattera <matteravalerio@gmail.com>
2024-01-02 10:49:02 +0100

    cleanup

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,16 +2,6 @@ import type { GlobalsQueryFragment, PageBlocksFieldsFragment, ReviewQueryFragmen
 
 export type THeadingType = "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
 
-export type TTextSize =
-  | "headingExtraSmall"
-  | "headingSmall"
-  | "headingMedium"
-  | "headingLarge"
-  | "headingExtraLarge"
-  | "contentLarge"
-  | "contentMedium"
-  | "contentSmall"
-
 export type TImage = {
   altText?: string
   sourceUrl?: string
@@ -21,34 +11,6 @@ export type TMenuItem = {
   path: string
   label: string
   cssClasses: string
-}
-
-export type TReview = {
-  slug: string
-  date: string
-  excerpt?: string
-  title: string
-  reviewFields?: {
-    vote?: number
-    hero?: {
-      sourceUrl: string
-    }
-  }
-  featuredImage?: {
-    node: {
-      altText: string
-      sourceUrl: string
-    }
-  }
-}
-
-export type TGlobals = {
-  socials?: {
-    facebook: string
-    instagram: string
-    psn: string
-    steam: string
-  }
 }
 
 export type TBlock = PageBlocksFieldsFragment["blocks"][number]
